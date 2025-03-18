2ab7cac11b565f01518cffa06099b4661f055d53
Naythankik <dolapokiki3ird@gmail.com>
2025-03-18 09:42:15 +0100

    - WIP for vercel

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -9,5 +9,8 @@ router.post('/login', login);
 router.post('/signup', create, createMail);
 router.post('/verify-account/:token', verifyAccount)
 router.post('/request-verification', requestVerification, requestVerificationMail)
+router.get('/testing', (req, res) => {
+    res.json("Working")
+})
 
 module.exports = router;
