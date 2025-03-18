630e1e5f02700de95317d89eddfaf03ede6871a9
Naythankik <dolapokiki3ird@gmail.com>
2025-03-18 09:51:45 +0100

    - WIP for vercel

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,7 +16,7 @@ const login = async (req, res) => {
     try {
         const {email, password} = value;
         const user = await User.findOne({email});
-        res.status(200).json({user})
+        res.status(200).json({success: true, user})
         return;
 
         if (!user) {
