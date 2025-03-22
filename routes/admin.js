3311c71d2c1332af6a5e4660e0ca6ebce74cb891
Naythankik <dolapokiki3ird@gmail.com>
2025-03-22 08:40:37 +0100

    - WIP

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,17 @@ const express = require("express");
 const { createSubscriber } = require("../src/controllers/subscribersController");
 const { createPage, deletePage, updatePage} = require("../src/controllers/pagesController");
 const upload = require('../src/helper/multer')
+const { editACategoryDocument, deleteACategoryDocument } = require("../src/controllers/adminController");
 
 const router = express.Router();
 
 router.post("/page/create", upload.fields([{ name: 'pageImage', maxCount: 1}, {name: 'pageCoverImage', maxCount: 1}]), createPage)
 router.put("/page/update/:id", updatePage)
 router.delete("/page/delete/:id", deletePage)
+
+router.put("/categories/:collection/update/:id", editACategoryDocument)
+router.delete("/categories/:collection/delete/:id", deleteACategoryDocument)
+
 router.post("/subscribers/create", createSubscriber);
 
 module.exports = router;
--- seeder.cjs
+++ seeder.cjs
@@ -1,7 +1,7 @@
 const userFactory = require('./src/database/factory/userFactory');
 const subscriber = require('./src/database/factory/subscriberFactory');
 const universal = require('./src/database/factory/universalFactory');
-const pageFactory =  require('./src/database/factory/pageFactory');
+const pageFactory = require('./src/database/factory/pageFactory');
 const User = require('./src/models/user');
 const Component = require('./src/models/component');
 const Stack = require('./src/models/stack');
@@ -15,56 +15,65 @@ require('dotenv').config();
 const connection = require("./config/database");
 connection();
 
+// Seeder functions
 const seederUsers = async () => {
     const users = await userFactory();
-
     try {
         await User.insertMany(users);
-        process.exit(0);
+        console.log('Users seeded successfully');
     } catch (err) {
-        console.error(err);
-        process.exit(1);
+        console.error('Error seeding users:', err);
+        throw err;
     }
 };
 
 const seederPages = async () => {
-    const pages = await pageFactory()
+    const pages = await pageFactory();
     try {
         await Page.insertMany(pages);
-        process.exit(0);
+        console.log('Pages seeded successfully');
     } catch (err) {
-        console.error(err)
-        process.exit(1);
+        console.error('Error seeding pages:', err);
+        throw err;
     }
 };
 
 const seedUniversal = async () => {
     const universals = [Industry, Component, Stack, Style, Type];
-    try{
+    try {
         for (let model of universals) {
             await model.insertMany(universal);
+            console.log(`${model.modelName} seeded successfully`);
         }
-        process.exit(0);
-    }catch(err){
-        console.error(err)
-        process.exit(1);
+    } catch (err) {
+        console.error('Error seeding universal models:', err);
+        throw err;
     }
-}
-
+};
 
 const seederSubscribers = async () => {
     try {
         await Subscriber.insertMany(subscriber);
+        console.log('Subscribers seeded successfully');
+    } catch (err) {
+        console.error('Error seeding subscribers:', err);
+        throw err;
+    }
+};
+
+// Run seeders sequentially
+const runSeeders = async () => {
+    try {
+        await seederUsers();
+        await seederSubscribers();
+        await seedUniversal();
+        await seederPages();
+        console.log('All seeders completed successfully!');
         process.exit(0);
     } catch (err) {
+        console.error('Seeder error:', err);
         process.exit(1);
     }
 };
 
-// seederUsers();
-// seederSubscribers()
-//
-// seedUniversal()
-// seederPages();
-
-
+runSeeders();
